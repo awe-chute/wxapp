8fab521f0396c1cf7f22f2bc48d8e1b1a779bdd5
awe-chute <114938692+awe-chute@users.noreply.github.com>
2025-02-22 00:01:09 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Loop through directories
 for dir in */; do
+  dir=${dir%/}
   if [ -d "$dir" ]; then
     echo "directory: $dir"
     cd "$dir"
@@ -10,22 +11,22 @@ for dir in */; do
     npm install -g pm2
 
     # Start services based on directory name
-    if [ "$dir" == "plants/" ]; then
+    if [ "$dir" == "plants" ]; then
       sudo pm2 start index.js --name "wxapp-plants" -- --host 0.0.0.0 --port 4000
       echo "Started wxapp-plants"
     fi
 
-    if [ "$dir" == "event-bus/" ]; then
+    if [ "$dir" == "event-bus" ]; then
       sudo pm2 start index.js --name "wxapp-eventbus" -- --host 0.0.0.0 --port 4005
       echo "Started wxapp-eventbus"
     fi
 
-    if [ "$dir" == "calculation/" ]; then
+    if [ "$dir" == "calculation" ]; then
       sudo pm2 start index.js --name "wxapp-calculation" -- --host 0.0.0.0 --port 4002
       echo "Started wxapp-calculation"
     fi
 
-    if [ "$dir" == "precipitation/" ]; then
+    if [ "$dir" == "precipitation" ]; then
       sudo pm2 start index.js --name "wxapp-precipitation" -- --host 0.0.0.0 --port 4001
       echo "Started wxapp-precipitation"
     fi
