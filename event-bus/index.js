9a9cd15ee855ba0395b765fad67b2c288612c1fa
awe-chute <114938692+awe-chute@users.noreply.github.com>
2025-02-21 23:57:50 -0700

    Update index.js

--- event-bus/index.js
+++ event-bus/index.js
@@ -23,6 +23,6 @@ app.post('/events', (req, res) => {
     res.send({ status: 'Successful!' });
 });
 
-app.listen(PORT, HOST () => {
+app.listen(PORT, HOST, () => {
     console.log(`Event Bus running at http://${HOST}:${PORT}`);
 });
